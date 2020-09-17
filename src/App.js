f597df43e134e30346446e89e3911fc49bbbfb7d
Yashas Reddy <yashasreddy@Yashass-MacBook-Air.local>
2020-09-17 13:14:42 +0530

    finished styling menu-items

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import HomePage from './pages/homepage/homepage.component';
+import { Route } from 'react-router-dom';
 import './App.css';
+
+
+function HatsPage() {
+  return (
+    <div>
+      <h1>Hats Page</h1>
+    </div>
+  )
+}
 
 function App() {
   return (
     <div className="App">
-      <HomePage />
+      <Route exact path='/' component={HomePage} />
+      <Route path='/shop/hats' component={HatsPage} />
     </div>
   );
 }
